a8bb5742993bed4c3356c6ec04ccfe0fbe6622bf
Pablo-Mallea <pablomallea02@gmail.com>
2022-06-28 18:02:30 -0300

    Agregando estilo

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,9 @@ let prod1 = 1000;
 let prod2 = 1500;
 let prod3 = 2000;
 
-function seleccion (param1, param2){
-    
+alert("que cantidad de productos que va a llevar:");
+function cuenta (param1, param2, param3){
+    let resultado = param1*prod1 + param2*prod2 + param3*prod3;
 }
+
+cuenta (+prompt("producto1"), +prompt("producto2"), +prompt("producto3"));
