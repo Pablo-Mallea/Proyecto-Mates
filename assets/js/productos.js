3653bee817f3fb23729e6a11e31bc69005f6c163
Pablo-Mallea <pablomallea02@gmail.com>
2022-07-04 14:40:06 -0300

    Entrega Final 2

--- assets/js/productos.js
+++ assets/js/productos.js
@@ -1,42 +1,42 @@
 const productos = [
     {
         id: 1,
-        imagen: "../assets/img/_DSC0195.jpg",
+        imagen: "../assets/img/producto.JPG",
         name: "Mate 1",
         categoria: "mate",
         precio: 1000,
     },
     {
         id: 2,
-        imagen: "../assets/img/_DSC0195.jpg",
+        imagen: "../assets/img/producto.JPG",
         name: "Mate 2",
         categoria: "mate",
         precio: 1500
     },
     {
         id: 3,
-        imagen: "../assets/img/_DSC0195.jpg",
+        imagen: "../assets/img/producto.JPG",
         name: "Mate 3",
         categoria: "mate",
         precio: 2000
     },
     {
         id: 4,
-        imagen: "../assets/img/_DSC0195.jpg",
+        imagen: "../assets/img/producto.JPG",
         name: "Yerba 1",
         categoria: "yerba",
         precio: 500
     },
     {
         id: 5,
-        imagen: "../assets/img/_DSC0195.jpg",
+        imagen: "../assets/img/producto.JPG",
         name: "Yerba 2",
         categoria: "yerba",
         precio: 800
     },
     {
         id: 6,
-        imagen: "../assets/img/_DSC0195.jpg",
+        imagen: "../assets/img/producto.JPG",
         name: "Yerba 3",
         categoria: "yerba",
         precio: 800
