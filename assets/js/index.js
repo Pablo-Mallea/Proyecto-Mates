3d99746492c1ed28d7927a18b038ddca8099663e
Pablo-Mallea <pablomallea02@gmail.com>
2022-07-08 11:40:25 -0300

    Funcion filtros

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-//Entrega Final 2
+//Incorporando Librerias
 
 let contenedor_productos = document.getElementById("productos")
 let contenedor_carrito = document.getElementById("carrito")
@@ -12,7 +12,6 @@ cargoProductos();
 
 //Cargo los productos del localStorage
 function cargarCarrito() {
-
     carrito = JSON.parse(localStorage.getItem("StorageProductos")) || localStorage.setItem("StorageProductos", JSON.stringify(carrito));
 }
 
@@ -42,24 +41,18 @@ function cargoProductos() {
 
 function seleccionProductos(id_seleccionado) {
     let cant1 = document.getElementById("cant1").value;
-    
     let cant2 = document.getElementById("cant2").value;
-    
     let cant3 = document.getElementById("cant3").value;
-    
     let cant4 = document.getElementById("cant4").value;
-    
     let cant5 = document.getElementById("cant5").value;
-    console.log(cant5);
     let cant6 = document.getElementById("cant6").value;
-    console.log(cant6);
-    
-    cant1 > 0 ? cantidad = cant1 : false
-    cant2 > 0 ? cantidad = cant2 : false
-    cant3 > 0 ? cantidad = cant3 : false
-    cant4 > 0 ? cantidad = cant4 : false
-    cant5 > 0 ? cantidad = cant5 : false
-    cant6 > 0 ? cantidad = cant6 : false
+
+    cant1 > 0 ? cantidad = +(cant1) : false
+    cant2 > 0 ? cantidad = +(cant2) : false
+    cant3 > 0 ? cantidad = +(cant3) : false
+    cant4 > 0 ? cantidad = +(cant4) : false
+    cant5 > 0 ? cantidad = +(cant5) : false
+    cant6 > 0 ? cantidad = +(cant6) : false
 
     let indice = id_seleccionado - 1;
 
@@ -68,7 +61,7 @@ function seleccionProductos(id_seleccionado) {
         ...productos[indice],
         cantidad: cantidad
     }
-
+    console.log(objeto_seleccionado)
     if (!carrito.some((el) => el.id == id_seleccionado)) { //Sino existe el producto en el carrito lo agrego
 
         carrito.push(objeto_seleccionado);
@@ -76,6 +69,13 @@ function seleccionProductos(id_seleccionado) {
         localStorage.setItem("StorageProductos", JSON.stringify(carrito));
         location.reload()
 
+    } else {
+        Swal.fire({ //Libreria sweetalert
+            title: 'Cuidado!',
+            text: 'Debes eliminar el producto del carrito primero',
+            icon: 'info',
+            confirmButtonText: 'ok'
+        })
     }
 }
 
@@ -119,7 +119,7 @@ let cuotas = 0;
 calcular.onclick = () => {
 
     for (let i = 0; i < carrito.length; i++) {
-        resultado = carrito[i].precio*carrito[i].cantidad + resultado;
+        resultado = carrito[i].precio * carrito[i].cantidad + resultado;
     }
     console.log(resultado)
     let calculo = document.getElementById("calculo")
@@ -151,4 +151,53 @@ pagar.onclick = () => {
 
         contenedor_cuotas.innerHTML = `<p>Total $${cuenta} en ${cuotas} cuotas de $ ${cu}</p>`
     }
+}
+
+//Filtrar productos
+let todos = document.getElementById("btn-todos")
+let mates = document.getElementById("btn-mates")
+let yerba = document.getElementById("btn-yerba")
+let bombilla = document.getElementById("btn-bombilla")
+
+todos.onclick = () => {
+    filtros("todos");
+}
+mates.onclick = () => {
+    filtros("mate");
+}
+yerba.onclick = () => {
+    filtros("yerba");
+}
+bombilla.onclick = () => {
+    filtros("bombilla");
+}
+
+function filtros(e) {
+    if (e == "todos") {
+        contenedor_productos.innerHTML = ``;
+        cargoProductos();
+    } else {
+        /* console.log(e); */
+        contenedor_productos.innerHTML = ``;
+        productos.forEach(productos => {
+
+            productos.categoria == e ? /* console.log(productos.categoria) */
+                contenedor_productos.innerHTML += `
+            <div class="card">
+                <div class="foto_producto">
+                    <img class="imagen" src="${productos.imagen}">
+                </div>
+                <div class="card_body">
+                    <p>${productos.name}</p>
+                    <p>$ ${productos.precio}</P>
+                    <div class="flex-row">
+                        <input type="numero" id="cant${productos.id}" placeholder="Cantididad..."> 
+                        <button class="btn-comprar" onClick="seleccionProductos(${productos.id})">Comprar</button>
+                    </div>
+                </div>
+            </div>
+        ` : false
+        });
+    }
+
 }
